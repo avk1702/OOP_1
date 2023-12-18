992db10cbd54e475f5ba2d606965894caa561fdb
avk1702 <avk1702@bk.ru>
2023-12-18 16:14:02 +0500

    2

---
+++ HotDrink.java
@@ -0,0 +1,27 @@
+package Java.OOP_1;
+
+public class HotDrink {
+    private String name;
+    private int volume;
+
+    public HotDrink(String name, int volume) {
+        this.name = name;
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+}
